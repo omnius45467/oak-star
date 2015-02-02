b57c78da2a5fc4ee3e08c033e2f3fcc6361f910d
sammyboy45467 <jeremy.robles56@gmail.com>
2015-02-02 09:17:27 -0800

    modified the video section

--- wp-content/themes/Nature-wp-theme/NatureWPTheme_v1.9/functions/m-parallax.php
+++ wp-content/themes/Nature-wp-theme/NatureWPTheme_v1.9/functions/m-parallax.php
@@ -12,28 +12,23 @@ global $nature_mt; ?>
                 <div class="newsletter-box">
                         <ul class="og-grid">
                             <?php
-                            //echo "<li class='parallax-item'>";
-                            //echo do_shortcode('[types field="youtube-video-url" seporator=" " output="raw"][/types]');
-                            //echo do_shortcode('[video_lightbox_youtube video_id="youtube-video-url" width="100%" height="100%" anchor="youtube-video-url"]');
-                            //echo "</li>";
-                            //$youtube = types_render_field("youtube_videos", array("raw"=>"true","separator"=>";"));
-                            //for ($youtube_url = 1;$youtube_url <= 9; $youtube_url++) {
-                            //echo "<li class='parallax-item'>Youtube Video:";
-                            //echo "</li>";
-                            //}
-                            
+                            foreach (get_oak_star_videos() as $video):
+                                ?>
+                                <?php
+                                echo "<li class='parallax-item'>";
+                                echo $video['title'];
+                                echo $video['desc'];
+                                echo do_shortcode('
+                            [video_lightbox_youtube video_id="$video["link"]" width="100%" height="100%" anchor="$video["title"]"]
+                            ');
+                                echo "</li>";
+                                ?>
+                                <br/>
+                            <?php
+                            endforeach;
                             ?>
                         </ul>
-                    <?php
-                        foreach (get_oak_star_videos() as $video):
-                    ?>
-                            <?php
-                            echo do_shortcode("[video_lightbox_youtube video_id='<?php echo $video['link']; ?>' width='100%' height='100%' anchor='<?php echo $video['link']; ?>']");
-                                ?>
-                            <!--                            <iframe width="560" height="315" src="--><?php //echo $video['link']; ?><!--" frameborder="0" allowfullscreen></iframe>-->
-                    <?php
-                        endforeach;
-                    ?>
+
                 </div>
             </div>
         </div>
